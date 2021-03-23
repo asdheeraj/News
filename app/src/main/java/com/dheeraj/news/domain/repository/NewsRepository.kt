bf6de53f447606b24f3d0fe1798250c3d124e66c
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-23 23:05:07 +0530

    Domain Repository updated with getLikes and getComments functionality

--- app/src/main/java/com/dheeraj/news/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/dheeraj/news/domain/repository/NewsRepository.kt
@@ -1,8 +1,14 @@
 package com.dheeraj.news.domain.repository
 
+import com.dheeraj.news.domain.entity.Comments
+import com.dheeraj.news.domain.entity.Likes
 import com.dheeraj.news.domain.entity.NewsArticle
 import com.dheeraj.news.util.Resource
 
 interface NewsRepository {
     suspend fun getTopHeadlines() : Resource<List<NewsArticle>>
+
+    suspend fun getLikes() : Resource<Likes>
+
+    suspend fun getComments() : Resource<Comments>
 }
