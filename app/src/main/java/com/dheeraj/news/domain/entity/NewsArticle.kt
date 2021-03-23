390726fb34eddcf2d01332b0ec2c623d208c7fa3
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 00:18:33 +0530

    NewsArticle Entity updated to hold the article Id, likes and comments

--- app/src/main/java/com/dheeraj/news/domain/entity/NewsArticle.kt
+++ app/src/main/java/com/dheeraj/news/domain/entity/NewsArticle.kt
@@ -1,11 +1,15 @@
 package com.dheeraj.news.domain.entity
 
 data class NewsArticle(
+    val articleId: String,
     val description: String,
     val author: String,
-    val imageUrl: String
+    val imageUrl: String,
+    var likes: Int? = null,
+    var comments: Int? = null
 ) {
     companion object {
-        const val PLACEHOLDER_IMAGE_URL = "https://cbsnews2.cbsistatic.com/hub/i/r/2021/03/23/2fe4a705-1df3-46f4-a7f4-066c90d3ff7b/thumbnail/1200x630/243bbb0c18ab20a652fc73828e2c326c/gettyimages-1308536283.jpg"
+        const val PLACEHOLDER_IMAGE_URL =
+            "https://cbsnews2.cbsistatic.com/hub/i/r/2021/03/23/2fe4a705-1df3-46f4-a7f4-066c90d3ff7b/thumbnail/1200x630/243bbb0c18ab20a652fc73828e2c326c/gettyimages-1308536283.jpg"
     }
 }
