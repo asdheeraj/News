7e47f88195e7f3401729188b15470bf5c2168ae8
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 00:42:37 +0530

    SafeApi call added to Handle success and error scenarios at a single place

---
+++ app/src/main/java/com/dheeraj/news/util/SafeApiCall.kt
@@ -0,0 +1,27 @@
+package com.dheeraj.news.util
+
+import retrofit2.Response
+
+suspend fun <T : Any> safeApiCall(
+    call: suspend () -> Response<T>
+): Resource<T> = try {
+    val response = call.invoke()
+    if (response.isSuccessful) {
+        responseToResource(response)
+    } else {
+        Resource.Error(message = response.message())
+    }
+} catch (e: Exception) {
+    Resource.Error(message = e.message ?: "Unknown Exception")
+}
+
+private fun <T : Any> responseToResource(
+    response: Response<T>
+): Resource<T> {
+    if (response.isSuccessful) {
+        return response.body()?.let { data ->
+            Resource.Success(data)
+        } ?: Resource.Error(response.message())
+    }
+    return Resource.Error(response.message())
+}
