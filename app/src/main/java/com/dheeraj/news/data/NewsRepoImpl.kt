1ca52f0fd9f6d4d606dde79eefd46b979a841e7c
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 01:26:38 +0530

    Implementation added for Domain Layer News Repository

---
+++ app/src/main/java/com/dheeraj/news/data/NewsRepoImpl.kt
@@ -0,0 +1,42 @@
+package com.dheeraj.news.data
+
+import com.dheeraj.news.data.mappers.NewsArticleMapper
+import com.dheeraj.news.data.model.CommentsResponse
+import com.dheeraj.news.data.model.LikesResponse
+import com.dheeraj.news.data.model.NewsHeadlinesApiResponse
+import com.dheeraj.news.data.repository.dataSource.NewsRemoteDataSource
+import com.dheeraj.news.domain.entity.NewsArticle
+import com.dheeraj.news.domain.repository.NewsRepository
+import com.dheeraj.news.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+class NewsRepoImpl @Inject constructor(
+    private val newsRemoteDataSource: NewsRemoteDataSource
+) : NewsRepository {
+
+    override suspend fun getTopHeadlines(): Resource<List<NewsArticle>> {
+        return newsRemoteDataSource.getNewsTopHeadlines().let { response  ->
+            return@let when (response) {
+                is Resource.Success -> getNewsArticles(response)
+                else -> Resource.Error(message = response.message ?: "Error Fetching Top Headlines", data = arrayListOf())
+            }
+        }
+    }
+
+    override suspend fun getLikes(articleId: String): Flow<Resource<LikesResponse>> {
+        return flowOf(newsRemoteDataSource.getLikes(articleId))
+    }
+
+    override suspend fun getComments(articleId: String): Flow<Resource<CommentsResponse>> {
+        return flowOf(newsRemoteDataSource.getComments(articleId))
+    }
+
+    private fun getNewsArticles(newsHeadlinesApiResponse: Resource<NewsHeadlinesApiResponse>): Resource<List<NewsArticle>> {
+        val articles = newsHeadlinesApiResponse.data?.articles?.map { article ->
+            NewsArticleMapper().mapToEntity(article)
+        } ?: arrayListOf()
+        return Resource.Success(articles)
+    }
+}
