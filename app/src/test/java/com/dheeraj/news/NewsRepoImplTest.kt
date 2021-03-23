1128fc19f95e6a1819d1bca2080184e1b822491c
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 02:56:26 +0530

    Unit test cases added for News Repository

--- app/src/test/java/com/dheeraj/news/NewsRepoImplTest.kt
+++ app/src/test/java/com/dheeraj/news/NewsRepoImplTest.kt
@@ -1,9 +1,12 @@
-package com.dheeraj.news.dataSourceImpl
+package com.dheeraj.news
 
+import com.dheeraj.news.data.NewsRepoImpl
 import com.dheeraj.news.data.api.NewsApiService
 import com.dheeraj.news.data.repository.dataSource.NewsRemoteDataSource
 import com.dheeraj.news.data.repository.dataSourceImpl.NewsRemoteDataSourceImpl
+import com.dheeraj.news.domain.repository.NewsRepository
 import com.google.common.truth.Truth.*
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -14,8 +17,9 @@ import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class NewsRemoteDataSourceImplTest {
+class NewsRepoImplTest {
 
+    private lateinit var newsRepository: NewsRepository
     private lateinit var newsRemoteDataSource: NewsRemoteDataSource
     private lateinit var mockWebServer: MockWebServer
     private lateinit var newsApiService: NewsApiService
@@ -29,6 +33,7 @@ class NewsRemoteDataSourceImplTest {
             .build()
             .create(NewsApiService::class.java)
         newsRemoteDataSource = NewsRemoteDataSourceImpl(newsApiService)
+        newsRepository = NewsRepoImpl(newsRemoteDataSource)
     }
 
     private fun enqueueMockResponse(fileName: String) {
@@ -44,28 +49,29 @@ class NewsRemoteDataSourceImplTest {
     fun getNewsTopHeadlines_sentRequest_receivedExpected() {
         runBlocking {
             enqueueMockResponse("newsresponse.json")
-            val response = newsRemoteDataSource.getNewsTopHeadlines()
+            val response = newsRepository.getTopHeadlines()
             assertThat(response.data).isNotNull()
-            assertThat(response.data?.articles?.size).isEqualTo(20)
+            assertThat(response.data?.size).isEqualTo(20)
         }
     }
 
     @Test
     fun getLikes_sentRequest_receivedExpected() {
         runBlocking {
-            enqueueMockResponse("likes.json")
-            val response = newsRemoteDataSource.getLikes(articleId = "www.theverge.com-2020-7-21-21332300-nikon-z5-full-frame-mirrorless-camera-price-release-date-specs-index.html")
-            assertThat(response.data).isNotNull()
+            newsRepository.getLikes(articleId = "www.theverge.com-2020-7-21-21332300-nikon-z5-full-frame-mirrorless-camera-price-release-date-specs-index.html")
+                .collect { response ->
+                    assertThat(response.data).isNotNull()
+                }
         }
     }
 
     @Test
     fun getComments_sentRequest_receivedExpected() {
         runBlocking {
-            enqueueMockResponse("comments.json")
-            val response = newsRemoteDataSource.getComments(articleId = "www.theverge.com-2020-7-21-21332300-nikon-z5-full-frame-mirrorless-camera-price-release-date-specs-index.html")
-            assertThat(response.data).isNotNull()
+            newsRepository.getComments(articleId = "www.theverge.com-2020-7-21-21332300-nikon-z5-full-frame-mirrorless-camera-price-release-date-specs-index.html")
+                .collect { response ->
+                    assertThat(response.data).isNotNull()
+                }
         }
     }
-
 }
