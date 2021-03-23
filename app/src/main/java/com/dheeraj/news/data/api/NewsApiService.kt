51f9bbd003d70bc4e7248ff99ddfb9468f3a29d8
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 00:35:20 +0530

    functions added with return type and suspend key word for NewsApiService

--- app/src/main/java/com/dheeraj/news/data/api/NewsApiService.kt
+++ app/src/main/java/com/dheeraj/news/data/api/NewsApiService.kt
@@ -1,6 +1,10 @@
 package com.dheeraj.news.data.api
 
 import com.dheeraj.news.BuildConfig
+import com.dheeraj.news.data.model.CommentsResponse
+import com.dheeraj.news.data.model.LikesResponse
+import com.dheeraj.news.data.model.NewsHeadlinesApiResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.http.Url
@@ -8,14 +12,14 @@ import retrofit2.http.Url
 interface NewsApiService {
 
     @GET("v2/top-headlines")
-    fun getNewsTopHeadlines(
+    suspend fun getNewsTopHeadlines(
         @Query("country") country: String = "us",
         @Query("apiKey") apiKey: String = BuildConfig.API_KEY
-    )
+    ) : Response<NewsHeadlinesApiResponse>
 
     @GET
-    fun getLikes(@Url url: String)
+    suspend fun getLikes(@Url url: String) : Response<LikesResponse>
 
     @GET
-    fun getComments(@Url url: String)
+    suspend fun getComments(@Url url: String) : Response<CommentsResponse>
 }
