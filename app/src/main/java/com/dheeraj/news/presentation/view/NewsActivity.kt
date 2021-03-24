c47e222a7aef76519d45b1ede7f0efb8c23f34de
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 10:41:15 +0530

    Main Activity Renamed to News Activity and moved to view package of the presentation layer

--- app/src/main/java/com/dheeraj/news/presentation/view/NewsActivity.kt
+++ app/src/main/java/com/dheeraj/news/presentation/view/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.dheeraj.news
+package com.dheeraj.news.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dheeraj.news.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
