49d6448c779dc7ede7def20009351d0f7d815af9
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 17:33:09 +0530

    View Model added to News Activity to handle various News Functionalities.

--- app/src/main/java/com/dheeraj/news/presentation/view/NewsActivity.kt
+++ app/src/main/java/com/dheeraj/news/presentation/view/NewsActivity.kt
@@ -2,11 +2,20 @@ package com.dheeraj.news.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.dheeraj.news.R
+import com.dheeraj.news.presentation.viewmodel.NewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NewsActivity : AppCompatActivity() {
+
+    private val newsViewModel: NewsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
     }
+
+    fun getViewModelInstance() = newsViewModel
 }
