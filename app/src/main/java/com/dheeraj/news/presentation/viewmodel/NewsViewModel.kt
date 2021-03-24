7919418beb6548b74a042304ba6de1f593363a47
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 11:25:29 +0530

    News View Model added  to the Presentation Layer

---
+++ app/src/main/java/com/dheeraj/news/presentation/viewmodel/NewsViewModel.kt
@@ -0,0 +1,45 @@
+package com.dheeraj.news.presentation.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dheeraj.news.domain.entity.NewsArticle
+import com.dheeraj.news.domain.usecase.GetLikesAndCommentsUseCase
+import com.dheeraj.news.domain.usecase.GetNewsTopHeadlinesUseCase
+import com.dheeraj.news.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel @Inject constructor(
+    private val getNewsTopHeadlinesUseCase: GetNewsTopHeadlinesUseCase,
+    private val getLikesAndCommentsUseCase: GetLikesAndCommentsUseCase,
+    private val dispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    val newsArticlesLiveData: MutableLiveData<Resource<List<NewsArticle>>> = MutableLiveData()
+    val newsArticleLiveData: MutableLiveData<Resource<NewsArticle>> = MutableLiveData()
+
+    init {
+        getNewsTopHeadlines()
+    }
+
+    fun getNewsTopHeadlines() = viewModelScope.launch(dispatcher) {
+        with(newsArticlesLiveData) {
+            postValue(Resource.Loading())
+            postValue(getNewsTopHeadlinesUseCase.execute())
+        }
+    }
+
+    fun getLikesAndComments(article: NewsArticle) = viewModelScope.launch(dispatcher) {
+        with(newsArticleLiveData) {
+            postValue(Resource.Loading())
+            getLikesAndCommentsUseCase.execute(article).collect { articleResponse ->
+                postValue(articleResponse)
+            }
+        }
+    }
+}
