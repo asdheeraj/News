68c3d5d857945c7851c7cbe053ed3766decc9b0c
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 16:04:23 +0530

    Fake News repository added with valid values

--- app/src/test/java/com/dheeraj/news/data/repository/FakeNewsRepository.kt
+++ app/src/test/java/com/dheeraj/news/data/repository/FakeNewsRepository.kt
@@ -1,10 +1,7 @@
 package com.dheeraj.news.data.repository
 
-import com.dheeraj.news.data.model.Article
 import com.dheeraj.news.data.model.CommentsResponse
 import com.dheeraj.news.data.model.LikesResponse
-import com.dheeraj.news.data.model.NewsHeadlinesApiResponse
-import com.dheeraj.news.data.repository.dataSource.NewsRemoteDataSource
 import com.dheeraj.news.domain.entity.NewsArticle
 import com.dheeraj.news.domain.repository.NewsRepository
 import com.dheeraj.news.util.Resource
@@ -28,8 +25,14 @@ class FakeNewsRepository : NewsRepository {
     private fun getFakeApiResponse(): Resource<List<NewsArticle>> {
 
         return Resource.Success(data = arrayListOf(
-            NewsArticle(),
-            NewsArticle()
+            NewsArticle(articleId = "https://www.cbsnews.com/live-updates/boulder-shooting-colorado-2021-03-23/",
+            description = "One of the 10 people killed was a police officer, and a suspect is in custody.",
+            author = "Victoria Albert",
+            imageUrl = "https://cbsnews2.cbsistatic.com/hub/i/r/2021/03/23/2fe4a705-1df3-46f4-a7f4-066c90d3ff7b/thumbnail/1200x630/243bbb0c18ab20a652fc73828e2c326c/gettyimages-1308536283.jpg"),
+            NewsArticle(articleId = "https://www.cbbcnews.com/live-updates/boulder-shooting-colorado-2021-03-23/",
+                description = "One of the 10 people killed was a police officer, and a suspect is in custody.",
+                author = "Victoria Albert Junior",
+                imageUrl = "https://cbsnews2.cbsistatic.com/hub/i/r/2021/03/23/2fe4a705-1df3-46f4-a7f4-066c90d3ff7b/thumbnail/1200x630/243bbb0c18ab20a652fc73828e2c326c/gettyimages-1308536283.jpg")
         ))
     }
 
