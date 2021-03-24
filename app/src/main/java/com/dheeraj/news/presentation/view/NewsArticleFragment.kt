1e58ce7b9788bae391ad3fd3cf4e0139c7226c67
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-24 18:55:19 +0530

    News Article Fragment added to show the details of the Article selected

---
+++ app/src/main/java/com/dheeraj/news/presentation/view/NewsArticleFragment.kt
@@ -0,0 +1,73 @@
+package com.dheeraj.news.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.dheeraj.news.R
+import com.dheeraj.news.databinding.FragmentNewsArticleBinding
+import com.dheeraj.news.presentation.viewmodel.NewsViewModel
+import com.dheeraj.news.util.Resource
+
+class NewsArticleFragment : Fragment() {
+
+    private val newsViewModel by lazy {initViewModel()}
+    private lateinit var newsArticleBinding: FragmentNewsArticleBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        newsArticleBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_news_article, container, false)
+        return newsArticleBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeObservers()
+    }
+
+    private fun initViewModel(): NewsViewModel {
+        return when (activity) {
+            is NewsActivity -> {
+                (activity as NewsActivity).getViewModelInstance()
+            }
+            else -> {
+                throw IllegalArgumentException("Unknown Activity Instance")
+            }
+        }
+    }
+
+    private fun subscribeObservers() {
+        newsViewModel.newsArticleLiveData.observe(viewLifecycleOwner, Observer { newsResponse ->
+            when (newsResponse) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    newsArticleBinding.newsArticle = newsResponse.data
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    newsArticleBinding.newsArticle = newsResponse.data
+                    Toast.makeText(activity, newsResponse.message ?: "", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    private fun showProgressBar() {
+        newsArticleBinding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        newsArticleBinding.progressBar.visibility = View.GONE
+    }
+}
